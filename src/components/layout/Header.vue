e4cf21051b14fe5817f38f32431626fe75fa6060
sjs445 <sjs445@csu.fullerton.edu>
2019-09-30 12:36:23 -0700

    adding dist

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-row justify-between px-16">
-        <button class="shadow-lg">Button</button>
+        <button class="shadow-lg">Hamburger</button>
         <h1 class='text-6xl text-orange-700' style="text-shadow:1px 1px black;">Munchmaps</h1>
         <img src="./burgerLogo.png" alt="BurgerLogo" style="height: 100px; width: 100px;">
     </div>
