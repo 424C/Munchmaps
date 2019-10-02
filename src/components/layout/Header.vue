fdea74c5caf5b908dfa06ae1e7017c361631747f
sjs445 <sjs445@csu.fullerton.edu>
2019-10-02 12:43:57 -0700

    Adding Nav Menu

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,15 +1,33 @@
 <template>
+    <div>
+
     <div class="flex flex-row justify-between px-16">
-        <button class="shadow-lg">Hamburger</button>
+        <Slide width="200">
+            <div>
+            <h1 class="text-3xl">Distance</h1>
+            <input type="radio" name="distance" value="5miles">5 Miles
+            </div>
+            
+        </Slide>
         <h1 class='text-6xl text-orange-700' style="text-shadow:1px 1px black;">Munchmaps</h1>
         <img src="./burgerLogo.png" alt="BurgerLogo" style="height: 100px; width: 100px;">
     </div>
+    </div>
 </template>
 
+
+
 <script>
+
+import { Slide } from 'vue-burger-menu'
+
 export default {
-    name: "Header"
+    name: "Header",
+    components: {
+        Slide
+    }
 }
+
 </script>
 
 <style scoped>
